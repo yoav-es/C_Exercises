31f9a4d21736183e442cf5873185a83a95ae4bd0
yoav-es <70217988+yoav-es@users.noreply.github.com>
2022-03-09 15:39:13 +0200

    Update and rename main.c to rehab_calc.c

--- rehab_calc/rehab_calc.c
+++ rehab_calc/rehab_calc.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int daysCalc(int A[], int N, int X, int Y)
+int rehabCalc(int A[], int N, int X, int Y)
 {
     int min = 10000;
     for (int j = 0; j < (N - Y ) ; j++)
@@ -33,9 +33,9 @@ int main()
     int A2[] = {10,3,4,7};
     int A3[] = {4,2,5,4,3,5,1,4,2,7};
 
-    printf("the minimum cos of rehabilitation is %d\n", daysCalc(A1,4,2,2));
-    printf("the minimum cos of rehabilitation is %d\n", daysCalc(A2,4,2,3));
-    printf("the minimum cos of rehabilitation is %d\n", daysCalc(A3,9,3,2));
+    printf("the minimum cos of rehabilitation is %d\n", rehabCalc(A1,4,2,2));
+    printf("the minimum cos of rehabilitation is %d\n", rehabCalc(A2,4,2,3));
+    printf("the minimum cos of rehabilitation is %d\n", rehabCalc(A3,9,3,2));
 }
 
 
